63053bf17954834e28a111fc8d6ba33be6ee2ad7
Alex Calvert <alexanderajcalvert@gmail.com>
2021-06-29 17:09:17 -0700

    commit tree f27f0d2cef80af4a1ca7414e7faa2c10a86d7ee9

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 def hopper
 	programmer_hash = 
@@ -16,6 +17,7 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +59,7 @@ def dennis_ritchies_language
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +86,10 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash
 end
 
 def changing_alan
@@ -103,7 +111,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +136,8 @@ def adding_to_dennis
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
+
+# binding.pry
